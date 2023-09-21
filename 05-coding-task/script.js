938cab9cab8a8356cc3abff8c99a1bc161717973
Ibrahim Ciloglu <ibrahimciloglu.ic@gmail.com>
2023-09-21 11:22:44 +0200

    chore: add coding-task-05

---
+++ 05-coding-task/script.js
@@ -0,0 +1,40 @@
+//5.1
+const size = 25;
+let result;
+
+if (size > 10 && size < 20) {
+  result = "Greater than 10";
+} else if (size > 20) {
+  result = "Greater than 20";
+} else {
+  result = "Lower than 10";
+}
+
+console.log(result);
+// Returns Greater than 10 but should return Greater than 20
+
+// 5.2
+const oddEven = function (num) {
+  if (num % 2 == 0) {
+    console.log("Number is even.");
+  } else {
+    console.log("Number is odd.");
+  }
+};
+
+oddEven(3);
+
+// 5.3
+const oldYoung = function (num) {
+  if (num < 16 && num > 0) {
+    console.log("children");
+  } else if (num < 50 && num > 16) {
+    console.log("young person");
+  } else if (num > 50) {
+    console.log("elder person");
+  } else if ((num = isNaN || num < 0)) {
+    console.log("Invalid parameter");
+  }
+};
+
+oldYoung("ibrahim");
