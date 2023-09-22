d4ed0cee9ffe33301d9e4e2575c905ed7808a5f6
Ibrahim Ciloglu <ibrahimciloglu.ic@gmail.com>
2023-09-22 14:22:13 +0200

    chore: add coding-task-07
    exit

---
+++ 07-coding-task/script.js
@@ -0,0 +1,29 @@
+// 7.1 coding task
+//
+function removeItem(arr, num) {
+  let tempArr = structuredClone(arr);
+  tempArr.splice(num - 1, 1);
+
+  return tempArr;
+}
+
+let arr = ["BMW", "VW", "Audi", "Mercedes"];
+console.log(removeItem(arr, 2));
+console.log(arr);
+
+//7.2 coding task
+//
+let cars = ["BMW", "VW", 55, "Audi", 3, "Mercedes", "Lambo", 56, true, "obi"];
+let onlyStrings = [];
+
+function sumOfCharacters(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "string") {
+      onlyStrings.push(arr[i]);
+    }
+  }
+  let result = onlyStrings.join("").length;
+  return result;
+}
+
+console.log(sumOfCharacters(cars));
